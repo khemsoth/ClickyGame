468f63f2fc37eed8478c709e766737e2e47e48df
Keith Hemsoth <kevhemsoth@gmail.com>
2019-06-10 10:47:35 -0400

    added handleClick function

--- src/components/card.js
+++ src/components/card.js
@@ -1,21 +1,37 @@
 import React from "react";
 
+/*class Clicked extends Component {
+  state = {
+    clicked: false
+  }
+}*/
 function Card(props) {
 
+function handleClick(event) {
+ // const {clicked} = event.target;
+  console.log('button clicked');
+  for(var i = 0; i < props.characters.length; i++) {
+    var random = Math.floor(Math.random()*props.characters.length);
+    console.log(random);
+  };/*
+      this.state({
+      [clicked]: true
+    });*/
+}
 
    return(
     <div>
       <ul>
         {props.characters.map(item => (
-          <li key={item.id}>
+          <button onClick= {handleClick}>
+          <li key={item.id.toString()}>
             {item.name}<br></br>
             {item.image}
           </li>
+          </button>
         ))}
       </ul>
     </div>
     );
-  }
-
-
+}
 export default Card;
